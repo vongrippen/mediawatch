23eda04dac5d52771724dca617747831c5f106f8
Mike Cochran <mcochran@linux.com>
2020-04-25 04:36:21 +0000

    Multithreaded directory scanning (threads at the root watch level)

--- grifts/scan.go
+++ grifts/scan.go
@@ -18,39 +18,54 @@ var _ = grift.Namespace("plex", func() {
 	grift.Add("scan", func(c *grift.Context) error {
 		watchDirs := lib.GetWatchDirs()
 
-		for _, watchDir := range watchDirs {
-			err := filepath.Walk(watchDir.Path, func(path string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-				if info.IsDir() != true {
-					media := lib.AnalyzeFile(path, info)
-					media.ContentType = nulls.NewString(watchDir.ContentType)
-					if media.ContentType.String == "tv" {
-						pathList := strings.Split(path, string(filepath.Separator))
-						pathLength := len(pathList)
-						media.Season = nulls.NewString(pathList[pathLength-2])
-						media.Show = nulls.NewString(pathList[pathLength-3])
-					}
+		countWatchDirs := len(watchDirs)
+		finishChan := make(chan int)
+		finished := 0
 
-					// Search for existing records on this path and filename
-					existing := []models.MediaFile{}
-					models.DB.Select("id").Where("pathname = ?", media.Pathname).All(&existing)
+		for i, watchDir := range watchDirs {
+			go watchThisDir(watchDir, finishChan)
+			log.Printf("Started Thread #%v\n", i)
+		}
 
-					if len(existing) > 0 {
-						media.ID = existing[0].ID
-					}
-
-					// Save record
-					models.DB.Save(&media)
-				}
-				return nil
-			})
-			if err != nil {
-				log.Println(err)
-			}
+		for finished < countWatchDirs {
+			_ = <-finishChan
+			finished++
 		}
 
 		return nil
 	})
 })
+
+func watchThisDir(watchDir lib.WatchDir, finished chan int) {
+	err := filepath.Walk(watchDir.Path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() != true {
+			media := lib.AnalyzeFile(path, info)
+			media.ContentType = nulls.NewString(watchDir.ContentType)
+			if media.ContentType.String == "tv" {
+				pathList := strings.Split(path, string(filepath.Separator))
+				pathLength := len(pathList)
+				media.Season = nulls.NewString(pathList[pathLength-2])
+				media.Show = nulls.NewString(pathList[pathLength-3])
+			}
+
+			// Search for existing records on this path and filename
+			existing := []models.MediaFile{}
+			models.DB.Select("id").Where("pathname = ?", media.Pathname).All(&existing)
+
+			if len(existing) > 0 {
+				media.ID = existing[0].ID
+			}
+
+			// Save record
+			models.DB.Save(&media)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Println(err)
+	}
+	finished <- 1
+}
